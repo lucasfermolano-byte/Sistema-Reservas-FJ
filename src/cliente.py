19a361d1f85354c9e0fe4e6ecf5b32257bc26647
ingridpaolasanabria1994-star <ingrid.paola.sanabria.1994@gmail.com>
2026-05-04 23:08:22 -0500

    Update cliente.py

--- src/cliente.py
+++ src/cliente.py
@@ -1,1 +1,21 @@
-# clase cliente
+from excepciones import ClienteInvalidoError
+
+class Cliente:
+
+    def __init__(self, id, nombre, email):
+        self.id = id
+        self.set_nombre(nombre)
+        self.set_email(email)
+
+    def set_nombre(self, nombre):
+        if not nombre:
+            raise ClienteInvalidoError("El nombre no puede estar vacío")
+        self._nombre = nombre
+
+    def set_email(self, email):
+        if "@" not in email:
+            raise ClienteInvalidoError("Email inválido")
+        self._email = email
+
+    def __str__(self):
+        return f"{self._nombre} - {self._email}"
