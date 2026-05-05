b6cda96de163d2e7deca16b8fc6a924e0b98b79e
ingridpaolasanabria1994-star <ingrid.paola.sanabria.1994@gmail.com>
2026-05-04 23:09:13 -0500

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,1 +1,8 @@
-# clase logger
+from datetime import datetime
+
+class Logger:
+
+    @staticmethod
+    def log(mensaje):
+        with open("logs/logs.txt", "a") as archivo:
+            archivo.write(f"{datetime.now()} - {mensaje}\n")
