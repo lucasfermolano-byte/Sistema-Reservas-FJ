7ffafdfc854b3f044404bc2c0b4fd77b4d823800
ingridpaolasanabria1994-star <ingrid.paola.sanabria.1994@gmail.com>
2026-05-04 23:08:51 -0500

    Update reserva.py

--- src/reserva.py
+++ src/reserva.py
@@ -1,1 +1,26 @@
-# clase reserva
+from excepciones import ReservaError
+
+class Reserva:
+
+    def __init__(self, cliente, servicio, duracion):
+        if duracion <= 0:
+            raise ReservaError("Duración inválida")
+
+        self.cliente = cliente
+        self.servicio = servicio
+        self.duracion = duracion
+        self.estado = "Pendiente"
+
+    def confirmar(self):
+        self.estado = "Confirmada"
+
+    def cancelar(self):
+        self.estado = "Cancelada"
+
+    def procesar(self):
+        try:
+            costo = self.servicio.calcular_costo(self.duracion)
+            self.confirmar()
+            return costo
+        except Exception as e:
+            raise ReservaError("Error al procesar la reserva") from e
