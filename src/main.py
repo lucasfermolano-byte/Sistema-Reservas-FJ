82f307aa4c7b333ad08a0c475abe17ba58d8b18a
ingridpaolasanabria1994-star <ingrid.paola.sanabria.1994@gmail.com>
2026-05-07 19:20:09 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,34 @@
-# clase main
+from cliente import Cliente
+from sistema import Sistema
+from servicios import ReservaSala, AlquilerEquipo, Asesoria
+
+sistema = Sistema()
+
+# CLIENTES
+try:
+    c1 = Cliente(1, "Ana", "ana@email.com")
+    sistema.agregar_cliente(c1)
+
+    c2 = Cliente(2, "", "correo")  # error
+    sistema.agregar_cliente(c2)
+except Exception as e:
+    print("Error cliente:", e)
+
+# SERVICIOS
+sala = ReservaSala("Sala", 50)
+equipo = AlquilerEquipo("Laptop", 30)
+asesoria = Asesoria("Consultoría", 100)
+
+sistema.agregar_servicio(sala)
+sistema.agregar_servicio(equipo)
+sistema.agregar_servicio(asesoria)
+
+# RESERVAS
+sistema.crear_reserva(c1, sala, 2)
+sistema.crear_reserva(c1, equipo, 3)
+sistema.crear_reserva(c1, asesoria, 1)
+
+# ERROR
+sistema.crear_reserva(c1, sala, -5)
+
+print("Sistema ejecutado correctamente")
