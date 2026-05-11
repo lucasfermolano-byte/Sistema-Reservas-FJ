c343baffef840a1dfdd26b46673be5ff89cb6897
lucasfermolano-byte <lucasfermolano@gmail.com>
2026-05-10 19:50:10 -0500

    Implement duplicate ID check in agregar_cliente
    Added duplicate ID validation when adding a client.

--- src/sistema.py
+++ src/sistema.py
@@ -10,11 +10,23 @@ class Sistema:
 
     def agregar_cliente(self, cliente):
         try:
+            # Validación de duplicados (Requisito de estabilidad)
+            if any(c.id == cliente.id for c in self.clientes):
+                raise Exception(f"El ID {cliente.id} ya está registrado.")
             self.clientes.append(cliente)
-            Logger.log(f"Cliente agregado: {cliente}")
+        
         except Exception as e:
+            # Manejo del error
             Logger.log(f"Error al agregar cliente: {e}")
-
+            print(f"Error: {e}")
+            
+        else:
+            #Se ejecuta solo si todo salió bien
+            Logger.log(f"Éxito: Cliente {cliente.id} guardado correctamente.")
+            
+        finally:
+        
+            print(f"Intento de registro finalizado para el ID: {cliente.id}")
     def agregar_servicio(self, servicio):
         self.servicios.append(servicio)
 
