66794146ac6d4ede670c693a01f6e725f264ec9d
lucasfermolano-byte <lucasfermolano@gmail.com>
2026-05-11 03:23:01 -0500

    Update entidades.py

--- src/entidades.py
+++ src/entidades.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
+# Define la jerarquía de errores personalizados del sistema Software FJ
 
 class EntidadBase(ABC):
     def __init__(self, id_entidad):
         self.id_entidad = id_entidad
-
+# Permite una gestión de errores controlada.
     @abstractmethod
     def __str__(self):
         pass
